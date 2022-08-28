3d96b95ce5a2707c81e2e0d94b5067378bc42c5c
servuloccsilva <servuloccsilva@gmail.com>
2022-08-28 12:32:57 -0300

    Aula 28 - Operações aritmétivas

--- secao-4-estrutura-sequencial/primeiro_programa_java/src/Main.java
+++ secao-4-estrutura-sequencial/primeiro_programa_java/src/Main.java
@@ -1,23 +1,44 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+
+//		System.out.println("Hello World!");
+//		
+//		int y = 32;
+//		double x = 10.5689;
+//		System.out.println(y);
+//		System.out.printf("%.2f%n", x);
+//		System.out.printf("%.4f%n", x);
+//		Locale.setDefault(Locale.US);
+//		System.out.printf("%.4f%n", x);
+//		System.out.printf("RESULTADO = %.2f metros%n", x);
+//		
+//		String nome = "Maria";
+//		int idade = 31;
+//		double renda = 4000.0;
+//		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+
+//		Aula 26
+
+//		Scanner sc = new Scanner(System.in);
+////		Para digitar uma string
+////		String x;
+////		x = sc.next();
+////		System.out.println("Você digitou: " + x);
+//
+////		Para digitar um int
+//
+//		int x;
+//		x = sc.nextInt();
+//		System.out.println("VOcê digitou: " + x);
+//
+//		sc.close();
 		
-		int y = 32;
-		double x = 10.5689;
-		System.out.println(y);
-		System.out.printf("%.2f%n", x);
-		System.out.printf("%.4f%n", x);
-		Locale.setDefault(Locale.US);
-		System.out.printf("%.4f%n", x);
-		System.out.printf("RESULTADO = %.2f metros%n", x);
+//		Aula 28
 		
-		String nome = "Maria";
-		int idade = 31;
-		double renda = 4000.0;
-		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
 
 	}
 
